54b2034eea2a358e5c130203a0e6ba335e61d59d
aman <amanshitta18@gmail.com>
2025-03-25 19:49:26 +0530

    added GET and SET commands

--- app/commands.go
+++ app/commands.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"strings"
+	"sync"
 )
 
 func processCommand(c string) (func(net.Conn, []string) error, error) {
@@ -13,6 +14,10 @@ func processCommand(c string) (func(net.Conn, []string) error, error) {
 		return ping, nil
 	case "echo":
 		return echo, nil
+	case "set":
+		return set, nil
+	case "get":
+		return get, nil
 	default:
 		fmt.Println("Default case triggered :: ", c)
 		return nil, fmt.Errorf("not yet implemented")
@@ -33,6 +38,47 @@ func echo(c net.Conn, args []string) error {
 	resp := fmt.Sprintf("$%d\r\n%s\r\n", len(response), response)
 	fmt.Println("resp :: ", resp)
 	c.Write([]byte(resp))
+
+	return nil
+}
+
+type Map struct {
+	sync.Mutex
+	Data map[string]string
+}
+
+var sessionData = &Map{
+	Data: make(map[string]string),
+}
+
+func set(c net.Conn, args []string) error {
+
+	if len(args) != 2 {
+		return fmt.Errorf("ERR syntax error")
+	}
+	key := args[0]
+	value := args[1]
+
+	sessionData.Lock()
+	defer sessionData.Unlock()
+	sessionData.Data[key] = value
+
+	c.Write([]byte("+OK\r\n"))
+
+	return nil
+}
+
+func get(c net.Conn, args []string) error {
+	if len(args) == 0 {
+		return fmt.Errorf("GET requires an argument")
+	}
+
+	response, ok := sessionData.Data[args[0]]
+	if ok {
+		c.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(response), response)))
+	} else {
+		c.Write([]byte("$-1\r\n"))
+	}
 
 	return nil
 }
