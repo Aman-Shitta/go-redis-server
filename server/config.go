1d6daa8310cc022c80365644194d6572c64346fc
aman <amanshitta18@gmail.com>
2025-03-29 17:50:21 +0530

    fix the reading of RDB bytes with exp keys

--- server/config.go
+++ server/config.go
@@ -29,7 +29,7 @@ type Config struct {
 
 func NewConfig() *Config {
 	return &Config{
-		Dir:        "/tmp/redis",
+		Dir:        "",
 		Dbfilename: "db.rdb",
 	}
 }
--- server/rdbparser.go
+++ server/rdbparser.go
@@ -34,11 +34,18 @@ func ParseRDB(data []byte) {
 	// neeed to change into actual data rather than
 	// RDB_DB := make(map[int]map[string]string)
 
-	for b := ReadByte(&data); b != EOF; b = ReadByte(&data) {
+	for b := ReadByte(&data); b != EOF && len(data) > 0; b = ReadByte(&data) {
+
+		// Check directly for start of db
+		// skipping aux and meta data
 		if b == SELECTDB {
-			x := data[1:]
+			// read DB number
+			dnNumber := ReadByte(&data)
+			fmt.Println("DB number :", dnNumber)
+			var x []byte
+			copy(data, x)
 			var err error
-			dbmap, err := processDBKV(x)
+			dbmap, err := processDBKV(data)
 			if err != nil {
 				panic("something is wrong at : ParseRDB :: " + err.Error())
 			}
@@ -56,7 +63,7 @@ func ParseRDB(data []byte) {
 func processDBKV(bytes []byte) (map[string]string, error) {
 
 	// TODO: need to start reading from RESIZEDB
-	fmt.Println("Need to process : ", bytes)
+	fmt.Println("Need to process DB : ", bytes)
 
 	var mapData = make(map[string]string)
 
@@ -91,52 +98,58 @@ func bytesToTimestamp(b []byte) time.Time {
 func processByteToMap(bytes *[]byte, mapData *map[string]string) {
 
 	for {
+
 		b := ReadByte(bytes)
 
+		fmt.Println("read byte : ", b)
+		if b == EOF || b == SELECTDB {
+			utils.LogEntry("pink", "[+] rdb ended [+]")
+			return
+		}
+
+		var exp time.Time
+		if b == EXPIRETIMEMS {
+			// read next 8 bytes timestamp
+			var tsb []byte
+			for range 8 {
+				tsb = append(tsb, ReadByte(bytes))
+			}
+			exp = bytesToTimestamp(tsb)
+		}
+
 		if b == 0x00 {
-			k, v, exp := readKeyValue(bytes)
+			k, v := readKeyValue(bytes)
+			fmt.Println(k, v, exp)
 			fmt.Printf("%s:%v expired @ %s\n", k, v, exp.Format("2006-01-02 15:04:05"))
 			(*mapData)[k] = v
 			if !exp.IsZero() {
 				ExpKeys[k] = exp
 			}
 		}
-		if b == EOF || b == SELECTDB {
-			utils.LogEntry("pink", "[+] rdb ended [+]")
-			return
-		}
 	}
 }
 
 // ReadKeyValue reads a key-value pair from RDB
-func readKeyValue(data *[]byte) (string, string, time.Time) {
-	fmt.Println("rkv data :: ", data)
-	var key, value string
-	var ts time.Time
+func readKeyValue(data *[]byte) (string, string) {
 
-	key_size := ReadByte(data)
-
-	t := ReadByte(data)
-	if t == EXPIRETIMEMS {
-		// read next 8 bytes timestamp
-		var tsb []byte
-		for range 8 {
-			tsb = append(tsb, ReadByte(data))
-		}
-		ts = bytesToTimestamp(tsb)
-	} else {
-		// add the byte back to orignal data
-		(*data) = append([]byte{t}, (*data)...)
+	for _, b := range *data {
+		fmt.Printf("%02X ", b) // Ensures two-digit hex values
 	}
 
+	var key, value string
+
+	key_size := ReadByte(data)
+	fmt.Println("key_size :: ", key_size)
 	for range int(key_size) {
 		key += string(ReadByte(data))
 	}
+	fmt.Println("key : ", key)
 
 	value_size := ReadByte(data)
+	fmt.Println("value_size :: ", value_size)
 	for range int(value_size) {
 		value += string(ReadByte(data))
 	}
 
-	return key, value, ts
+	return key, value
 }
