6fb0d198c73b558881c1d093fd390598a3b78ff3
aman <amanshitta18@gmail.com>
2025-03-31 18:34:03 +0530

    read server rersp before sending data

--- server/handshake.go
+++ server/handshake.go
@@ -15,19 +15,21 @@ func InitiateHandshake(ip string, port int) error {
 	if err != nil {
 		return err
 	}
+	defer c.Close()
 
 	// send PING to master
 	ping := utils.ToArrayBulkString("PING")
 	c.Write([]byte(ping))
-
+	var d = make([]byte, 1024)
+	c.Read(d)
 	// send REPLCONF
 	replconf := utils.ToArrayBulkString("REPLCONF", "listening-port", "6380")
-	fmt.Println("replconf :: ", replconf)
 	c.Write([]byte(replconf))
+	c.Read(d)
 
 	replconf = utils.ToArrayBulkString("REPLCONF", "capa", "psync2")
-	fmt.Println("replconf :: ", replconf)
 	c.Write([]byte(replconf))
+	c.Read(d)
 
 	return nil
 }
